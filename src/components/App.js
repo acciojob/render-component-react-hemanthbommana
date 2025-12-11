c4f19ecd323d012aae4274fb3e74d03131cb5926
hemanthbommana <hemanthbommana2001@gmail.com>
2025-12-11 12:50:22 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import ParaComponent from "./Paracomponent";
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <ParaComponent/>
+    </div>
+    
   )
 }
 
---
+++ src/components/Paracomponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const ParaComponent = ()=>{
+    return <p>I am learning React. My life is getting better.</p>
+
+}
+
+export default ParaComponent;
